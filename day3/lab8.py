0f4bcc079eeadab35931cf1f0d7741962077e064
bhanu rai <bhanurai001@gmail.com>
2021-12-06 12:25:35 +0545

    complete homework

--- day3/lab8.py
+++ day3/lab8.py
@@ -1,2 +1,9 @@
 # Given a n-digit number. Find the sum of its digits.
 
+number=int(input("enter a number"))
+total=0
+while number>0:
+    d=number%10
+    total=total+d
+    number=number//10
+print("The sum of digits of the number entered is", total)
--- day3/lab9.py
+++ day3/lab9.py
@@ -2,3 +2,17 @@
 # username and password. If yes then print a message "Logged in Successfully", if not then print invalid credentials
 # for consecutive 3 times and if the limit exceeds than print "Attempt finished".
 
+username=input('enter your name')
+password=input('enter your password')
+for i in range (3):
+    print('enter your credentials')
+    username1=input('enter your:')
+    password1=input('enter password')
+    if username==username1 and password==password1:
+        print('you are successfully logged.')
+        break
+    else:
+        if(username!=username1 or password!=password1):
+            print('invalid credentials')
+else:
+    print('3 attempts finished')
